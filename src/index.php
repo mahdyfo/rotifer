5b7107c63bf3c26c5cc6f505cc48a3578b40c796
mahdyfo <mahdyfof@gmail.com>
2023-05-07 21:17:34 +0200

    add reproduction crossover test

--- src/index.php
+++ src/index.php
@@ -2,26 +2,14 @@
 
 require '../vendor/autoload.php';
 
-use GeneticAutoml\Activations\Activation;
-use GeneticAutoml\Models\Agent;
-use GeneticAutoml\Models\Neuron;
+use GeneticAutoml\Models\World;
 
+$population = 5;
 $inputs = [1, 0, 0];
 $outputs = [1];
 
-$agent1 = new Agent();
-$agent1->setActivation([Activation::class, 'sigmoid'])
-    ->createNeuron(Neuron::TYPE_INPUT, 3)
-    ->createNeuron(Neuron::TYPE_OUTPUT)
-    ->initRandomConnections();
+$world = new World();
+$world->createAgents($population, count($inputs), count($outputs));
 
-$agent2 = new Agent();
-$agent2->setActivation([Activation::class, 'sigmoid'])
-    ->createNeuron(Neuron::TYPE_INPUT, 3)
-    ->createNeuron(Neuron::TYPE_OUTPUT)
-    ->initRandomConnections();
-
-$agent1->reproduce($agent2);
-
-//$encoder = new BinaryEncoder();
-//echo $genome = $agent->getGenomeString($encoder, "\n");
+$child = \GeneticAutoml\Helpers\ReproductionHelper::crossover($world->getAgents()[0], $world->getAgents()[1]);
+var_dump($child);
