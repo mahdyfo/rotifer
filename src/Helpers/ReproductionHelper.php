07cbfa9bb9e8c543ff312c4447bb2798c6f53c36
mahdyfo <mahdyfof@gmail.com>
2023-05-07 21:16:22 +0200

    Finish crossover method

--- src/Helpers/ReproductionHelper.php
+++ src/Helpers/ReproductionHelper.php
@@ -7,18 +7,34 @@ use GeneticAutoml\Models\Agent;
 
 class ReproductionHelper
 {
-    public static function crossover(Agent $agent1, Agent $agent2): array
+    public static function crossover(Agent $agent1, Agent $agent2): Agent
     {
-        $binaryEncoder = new BinaryEncoder();
-        $genomes = [$agent1->getGenomeArray($binaryEncoder), $agent2->getGenomeArray($binaryEncoder)];
+        $genomes = [$agent1->getGenomeArray(), $agent2->getGenomeArray()];
         shuffle($genomes);
 
         $crossPoint = mt_rand(30, 70) / 100;
         $crossPoint = round($crossPoint * count($genomes[0]));
 
+        // Always > 0, at least 1
+        if ($crossPoint == 0 && count($genomes[0]) > 1) {
+            $crossPoint = 1;
+        }
+
+        // Always < max length, max count - 1
+        if ($crossPoint == count($genomes[0])) {
+            $crossPoint = count($genomes[0]) - 1;
+        }
+
         $part1 = array_slice($genomes[0], 0, $crossPoint);
         $part2 = array_slice($genomes[1], $crossPoint);
 
-        return array_merge($part1, $part2);
+        $newGenome = array_merge($part1, $part2);
+
+        return Agent::createFromGenome($newGenome);
+    }
+
+    public static function mutate(Agent $agent)
+    {
+
     }
 }
