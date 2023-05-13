c0fa17269abfd11acbbf5fec7a101ec93495294f
mahdyfo <mahdyfof@gmail.com>
2023-05-13 20:36:07 +0200

    change binary encoder to comply its interface

--- src/Encoders/BinaryEncoder.php
+++ src/Encoders/BinaryEncoder.php
@@ -9,24 +9,25 @@ class BinaryEncoder implements Encoder
 {
     private static $instance;
 
-    public static function getInstance(): BinaryEncoder
+    public static function getInstance(): self
     {
         if (!isset(self::$instance)) {
             self::$instance = new self();
         }
         return self::$instance;
     }
+
     /**
      * [input/neuron] [index] [neuron/output] [index] [weight]
      *      1-char    16-char       1-char    16-char 24-char
      * @param int $fromType
-     * @param $fromIndex
-     * @param $toType
-     * @param $toIndex
-     * @param $weight
+     * @param int $fromIndex
+     * @param int $toType
+     * @param int $toIndex
+     * @param float $weight
      * @return string
      */
-    public function encodeConnection(int $fromType, $fromIndex, $toType, $toIndex, $weight): string
+    public function encodeConnection(int $fromType, int $fromIndex, int $toType, int $toIndex, float $weight): string
     {
         // Input = 0, Neuron = 1
         $fromType = ($fromType == Neuron::TYPE_INPUT) ? 0 : 1;
@@ -43,13 +44,13 @@ class BinaryEncoder implements Encoder
             . str_pad(decbin($weight), 24, 0, STR_PAD_LEFT);
     }
 
-    public function decodeConnection(string $binaryGene): array
+    public function decodeConnection(string $encodedGene): array
     {
-        $fromType = substr($binaryGene, 0, 1) == "0" ? Neuron::TYPE_INPUT : Neuron::TYPE_HIDDEN;
-        $fromIndex = substr($binaryGene, 1, 16);
-        $toType = substr($binaryGene, 17, 1) == "0" ? Neuron::TYPE_HIDDEN : Neuron::TYPE_OUTPUT;
-        $toIndex = substr($binaryGene, 18, 16);
-        $weight = substr($binaryGene, 34, 24);
+        $fromType = substr($encodedGene, 0, 1) == "0" ? Neuron::TYPE_INPUT : Neuron::TYPE_HIDDEN;
+        $fromIndex = substr($encodedGene, 1, 16);
+        $toType = substr($encodedGene, 17, 1) == "0" ? Neuron::TYPE_HIDDEN : Neuron::TYPE_OUTPUT;
+        $toIndex = substr($encodedGene, 18, 16);
+        $weight = substr($encodedGene, 34, 24);
 
         return [
             'from_type' => $fromType,
