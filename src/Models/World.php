15afa0d08edbb4c11badf84bd83335978a961768
mahdyfo <mahdyfof@gmail.com>
2023-05-13 20:40:20 +0200

    Add reproduction and generation methods to world model

--- src/Models/World.php
+++ src/Models/World.php
@@ -50,13 +50,60 @@ class World
         return $this;
     }
 
-    public function reproduce(Agent $agent1, Agent $agent2)
+    public function reproduce(Agent $agent1, Agent $agent2): Agent
     {
-        $child = ReproductionHelper::crossover($agent1, $agent2);
+        $genome1 = $agent1->getGenomeArray();
+        $genome2 = $agent2->getGenomeArray();
 
-        // TODO: Mutation
+        // Crossover
+        [$genome1, $genome2] = ReproductionHelper::crossover($genome1, $genome2);
 
-        return $child;
+        // Dominance
+        $childGenome = ReproductionHelper::dominance($genome1, $genome2);
+
+        // Mutation
+        $childGenome = ReproductionHelper::mutate($childGenome);
+
+        // Translocation
+        $childGenome = ReproductionHelper::translocation($childGenome);
+
+        return Agent::createFromGenome($childGenome);
+    }
+
+    /**
+     * Run each agent with the data. The count of training rows is the age of the agent
+     * @param mixed $fitnessFunction Your custom function to calculate the fitness value for each agent
+     * @param array $data [ [[input1, input2, input3], [output1, output2]], [[inputX, inputY, inputZ], [outputA, outputB]] ]
+     * @throws Exception
+     */
+    //TODO: add synchronous param: True to run every agent simultaneously with others, False to age 1 agent completely before running the next
+    public function nextGeneration($fitnessFunction, array $data)
+    {
+        // Run all agents
+        $fitnessByAgentKey = [];
+        foreach ($this->agents as $key => $agent) {
+            // Each data
+            $fitness = 0;
+            foreach ($data as $row) {
+                // Step
+                $this->agents[$key]->step($row[0]);
+
+                // Feed data into fitness function
+                $otherAgents = $this->getAgents();
+                unset($otherAgents[$key]);
+                $fitness += $fitnessFunction($this->agents[$key], $row, $otherAgents);
+            }
+            $this->agents[$key]->setFitness($fitness);
+            $fitnessByAgentKey[] = [$fitness, $key];
+        }
+
+        // Sort agent keys by their fitness values
+        usort($fitnessByAgentKey, function($a, $b) {
+            return $b[0] > $a[0] ? 1 : -1;
+        });
+
+        return $fitnessByAgentKey;
+        // TODO: Reproduce
     }
 
     public function getGenomesString(Encoder $encoder = null, $geneIterationCallback = null, $geneSeparator = ';', $genomeSeparator = "\n"): string
