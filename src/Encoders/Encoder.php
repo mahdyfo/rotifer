1a55f7275be1c01dd1f612954e934f30cd266462
mahdyfo <mahdyfof@gmail.com>
2023-05-13 20:35:22 +0200

    more general and accurate encoder interface

--- src/Encoders/Encoder.php
+++ src/Encoders/Encoder.php
@@ -4,6 +4,6 @@ namespace GeneticAutoml\Encoders;
 
 interface Encoder
 {
-    public function encodeConnection(int $fromType, $fromIndex, $toType, $toIndex, $weight): string;
-    public function decodeConnection(string $binaryGene): array;
+    public function encodeConnection(int $fromType, int $fromIndex, int $toType, int $toIndex, float $weight): string;
+    public function decodeConnection(string $encodedGene): array;
 }
