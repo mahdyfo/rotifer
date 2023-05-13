adf1c510143941cc86f2cbc8fc788194bd3ff7e4
mahdyfo <mahdyfof@gmail.com>
2023-05-13 20:38:11 +0200

    Create json encoder

---
+++ src/Encoders/JsonEncoder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace GeneticAutoml\Encoders;
+
+class JsonEncoder implements Encoder
+{
+    private static $instance;
+
+    public static function getInstance(): self
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function encodeConnection(int $fromType, int $fromIndex, int $toType, int $toIndex, float $weight): string
+    {
+        return json_encode([$fromType, $fromIndex, $toType, $toIndex, $weight]);
+    }
+
+    public function decodeConnection(string $encodedGene): array
+    {
+        $decoded = json_decode($encodedGene);
+
+        return [
+            'from_type' => $decoded[0],
+            'from_index' => $decoded[1],
+            'to_type' => $decoded[2],
+            'to_index' => $decoded[3],
+            'weight' => $decoded[4],
+        ];
+    }
+}
