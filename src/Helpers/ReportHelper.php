1aa3dc257526d45c9dcbbc463e8b62b32effc965
mahdyfo <mahdyfof@gmail.com>
2023-07-19 01:13:35 +0200

    Add more info to report

--- src/Helpers/ReportHelper.php
+++ src/Helpers/ReportHelper.php
@@ -11,7 +11,9 @@ class ReportHelper
     {
         return [
             'fitness' => $agent->getFitness(),
+            'input_count' => count($agent->getNeuronsByType(Neuron::TYPE_INPUT)),
             'hidden_neurons_count' => count($agent->getNeuronsByType(Neuron::TYPE_HIDDEN)),
+            'output_count' => count($agent->getNeuronsByType(Neuron::TYPE_OUTPUT)),
             'connections_count' => count($agent->getGenomeArray()),
         ];
     }
