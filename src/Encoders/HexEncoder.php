71c82c3e66f838ab3a797767e1f4f1ae194d3694
mahdyfo <mahdyfof@gmail.com>
2023-07-18 20:50:58 +0200

    Fix base convert

--- src/Encoders/HexEncoder.php
+++ src/Encoders/HexEncoder.php
@@ -36,11 +36,52 @@ class HexEncoder implements Encoder
 
     public static function binToHex(string $binaryString): string
     {
-        return base_convert($binaryString, 2, 16);
+        return self::baseConvert($binaryString, 2, 16);
     }
 
     public static function hexToBin(string $hexString): string
     {
-        return base_convert($hexString, 16, 2);
+        return self::baseConvert($hexString, 16, 2);
+    }
+
+    /**
+     * Base convert large numbers
+     * https://www.php.net/manual/en/function.base-convert.php#127539
+     * @param $numString
+     * @param $fromBase
+     * @param $toBase
+     * @return string
+     */
+    public static function baseConvert($numString, $fromBase, $toBase): string
+    {
+        $chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+        $tostring = substr($chars, 0, $toBase);
+
+        $length = strlen($numString);
+        $result = '';
+        for ($i = 0; $i < $length; $i++)
+        {
+            $number[$i] = strpos($chars, $numString[$i]);
+        }
+        do
+        {
+            $divide = 0;
+            $newlen = 0;
+            for ($i = 0; $i < $length; $i++)
+            {
+                $divide = $divide * $fromBase + $number[$i];
+                if ($divide >= $toBase)
+                {
+                    $number[$newlen++] = (int)($divide / $toBase);
+                    $divide = $divide % $toBase;
+                } elseif ($newlen > 0)
+                {
+                    $number[$newlen++] = 0;
+                }
+            }
+            $length = $newlen;
+            $result = $tostring[$divide] . $result;
+        } while ($newlen != 0);
+        return $result;
     }
 }
