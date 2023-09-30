fac48bdd5ac02b585f9998a8af0056ab5b06b353
mahdyfo <mahdyfof@gmail.com>
2023-09-30 20:14:04 +0200

    Crc32 by default instead of md5

--- src/Encoders/WordEmbedding.php
+++ src/Encoders/WordEmbedding.php
@@ -4,7 +4,7 @@ namespace Rotifer\Encoders;
 
 class WordEmbedding
 {
-    public static function hashWord(string $word, $hashAlgo = 'md5'): array
+    public static function hashWord(string $word, $hashAlgo = 'crc32'): array
     {
         $hashed = hash($hashAlgo, strtolower($word));
         $embedded = [];
@@ -21,7 +21,7 @@ class WordEmbedding
      * @param bool $withBias whether to add the value 1 at first of each word array as bias
      * @return array returns an array of embedded words
      */
-    public static function hashSentence(string $sentence, string $hashAlgo = 'md5', string $delimiter = '\s+', bool $withBias = true): array
+    public static function hashSentence(string $sentence, string $hashAlgo = 'crc32', string $delimiter = '\s+', bool $withBias = true): array
     {
         $results = [];
         $words = preg_split('/' . $delimiter . '/', trim($sentence));
