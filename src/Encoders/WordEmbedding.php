8d881edf5d274bef86fd0a5fac0fa537d11fff5b
mahdyfo <mahdyfof@gmail.com>
2023-09-30 18:25:50 +0200

    add hash method to Word embedding

--- src/Encoders/WordEmbedding.php
+++ src/Encoders/WordEmbedding.php
@@ -4,13 +4,31 @@ namespace Rotifer\Encoders;
 
 class WordEmbedding
 {
-    public static function hash(string $word, $hashAlgo = 'md5'): array
+    public static function hashWord(string $word, $hashAlgo = 'md5'): array
     {
-        $hashed = hash($hashAlgo, $word);
+        $hashed = hash($hashAlgo, strtolower($word));
         $embedded = [];
         foreach (str_split($hashed) as $char) {
             $embedded[] = hexdec($char) / 15.0; // 15 because starts from 0 (0) to 15 (f)
         }
         return $embedded;
+    }
+
+    /**
+     * @param string $sentence
+     * @param string $delimiter the regex that is the splitter of words. Like \s for space chars.
+     * @param string $hashAlgo
+     * @param bool $withBias whether to add the value 1 at first of each word array as bias
+     * @return array returns an array of embedded words
+     */
+    public static function hashSentence(string $sentence, string $hashAlgo = 'md5', string $delimiter = '\s', bool $withBias = true): array
+    {
+        $results = [];
+        $words = preg_split('/' . $delimiter . '/', $sentence);
+        foreach ($words as $word) {
+            $embeddedWord = static::hashWord($word, $hashAlgo);
+            $results[] = $withBias ? [1, ...$embeddedWord] : $embeddedWord;
+        }
+        return $results;
     }
 }
